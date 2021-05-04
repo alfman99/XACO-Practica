399cc77577aad3c2d35797548910ede0257243e8
Alfredo <amanresamartinez@hotmail.com>
2021-05-05 00:46:10 +0200

    changed structure to classes

--- UDPClient_manresa-alfredo_bermejo-xavier_entrega_3.py
+++ UDPClient_manresa-alfredo_bermejo-xavier_entrega_3.py
@@ -1,121 +1,131 @@
 from socket import *
 import os
 
-def main():
+class Client:
 
-  clientSocket = socket(AF_INET, SOCK_DGRAM)
+  def __init__(self, serverAddr, serverPort, blockSize, timeout):
+    self.socket = socket(AF_INET, SOCK_DGRAM)
+    self.serverAddr = serverAddr
+    self.serverPort = serverPort
+    self.blockSize = blockSize
+    self.timeout = timeout
 
-  serverName = 'localhost'#input('ip servidor: ')
-  serverPort = 12000#input('puerto servidor: ')
+  def GET(self, filename: str, modo: str):
+    packet = self.createRRQ(filename, modo)
+    self.sendPacket(packet)
+    data = self.recvPacket(4)
+    packetType = data[:2]
+    file = None
+    if packetType == b'03':
+      file = open(filename, "wb")
+    elif packetType == b'05':
+      print('error')
+      return
 
-  condiciones = False
-  while not condiciones:
-      # Read in some text from the user
-      sendPack = input("Input (Example: GET hola.txt 32): ")
-
-      commands = sendPack.split(" ")
-      
-      method = commands[0]
-      filename = commands[1]
-      CHUNK_SIZE = int(commands[2])
-
-      if CHUNK_SIZE == 32 or CHUNK_SIZE == 64 or CHUNK_SIZE == 128 or CHUNK_SIZE == 256 or CHUNK_SIZE == 512 or CHUNK_SIZE == 1024 or CHUNK_SIZE == 2048:
-          condiciones = True
-
-  clientSocket.sendto(str(CHUNK_SIZE).encode(), (serverName, serverPort))
-
-  if method.upper() == 'GET':
-
-    clientSocket.sendto(createRRQ(filename, 'netascii'), (serverName, serverPort))
-
-    file = open(filename + ".2", "wb")
-
-    while True:
-
-      data, _ = clientSocket.recvfrom(CHUNK_SIZE)
-
+    while data:
       packetType = data[:2]
+      packetNum = int.from_bytes(data[2:4], 'big')
+      packetData = data[4:]
+      print('Num seq:', packetNum)
+      if packetType == b'03':
+        # handle DATA packet
+        packet = self.createACK(packetNum + 1)
+        self.sendPacket(packet)
+        file.write(packetData)
+        if len(data) < (self.blockSize + 4):
+          file.close()
+          break
+        else:
+          data = self.recvPacket(4)
 
-      if packetType == b'03':  # DATA
-        # guardamos datos
-        file.write(data[4:])
-        clientSocket.sendto(createACK(data[2:4]), (serverName, serverPort))
-      elif packetType == b'05':  # ERR
-        # error
-        file.close()
-        print('Code: Message:', data[2:4].decode(), data[4:].decode())
-
-      print("packet size recv:", len(data))
-
-      if len(data) < CHUNK_SIZE:
-        # sabemos que es el ultimo paquete enviado para que se para de guardar en el archivo
-        file.close()
-        break
-
-  elif method.upper() == 'PUT':
+      elif packetType == b'05':
+        # handle ERR packet
+        print('err')
     
-    clientSocket.sendto(createWRQ(filename, 'netascii'), (serverName, serverPort))
+    print('Got file:', filename, 'from:', self.serverAddr, self.serverPort)
 
-    serverACK, _ = clientSocket.recvfrom(CHUNK_SIZE)
+  def PUT(self, filename: str, modo: str):
+    packet = self.createWRQ(filename, modo)
+    self.sendPacket(packet)
 
-    print(serverACK)
+    serverACK = self.recvPacket(4)
+    packetType = serverACK[:2]
+
+    file = open(filename, 'rb')
+    data = file.read(self.blockSize)
 
     contador = 1
-    file = open(filename, "rb")
-    data = file.read(CHUNK_SIZE - 4)
+
     while data:
-      clientSocket.sendto(createDATA(contador, data), (serverName, serverPort))
-      contador = (contador % 65535) + 1
-      serverACK, _ = clientSocket.recvfrom(CHUNK_SIZE)
-      print(serverACK)
-      data = file.read(CHUNK_SIZE - 4)
       
-    if (os.path.getsize(filename) % CHUNK_SIZE) == 0:
-        clientSocket.sendto(createDATA(contador, ''), (serverName, serverPort))
-      
-    file.close()
+      packet = self.createDATA(contador, data)
+      self.sendPacket(packet)
 
-  clientSocket.close()
-      
+      serverACK = self.recvPacket(4)
 
-def createRRQ(nombrefichero, modo):
-  packet = b''
-  packet += b'01'
-  packet += nombrefichero.encode()
-  packet += b'0'
-  packet += modo.encode()
-  packet += b'0'
-  return packet
+      print('Num seq:', contador)
+      contador = (contador % pow(2, 16)) + 1
 
-def createWRQ(nombrefichero, modo):
-  packet = b''
-  packet += b'02'
-  packet += nombrefichero.encode()
-  packet += b'0'
-  packet += modo.encode()
-  packet += b'0'
-  return packet
+      data = file.read(self.blockSize)
 
-def createDATA(numbloque, data):
-  packet = b''
-  packet += b'03'
-  packet += "{:02d}".format(numbloque).encode()
-  packet += data  # is already encoded
-  return packet
-    
-def createACK(numbloque):
-  packet = b''
-  packet += b'04'
-  packet += "{:02d}".format(int(numbloque)).encode()
-  return packet
+      if data == b'' and self.extraEmpty(filename):
+        packet = self.createDATA(contador, b'')
+        self.sendPacket(packet)
 
-def createERROR(codigoerror, mensajeerror):
-  packet = b''
-  packet += b'05'
-  packet += codigoerror.encode()
-  packet += mensajeerror.encode()
-  packet += b'0'
-  return packet
+  def extraEmpty(self, filename: str) -> bool:
+    return (os.path.getsize(filename) % self.blockSize) == 0
+
+  def sendPacket(self, packet: bytearray) -> None:
+    self.socket.sendto(packet, (self.serverAddr, self.serverPort))
+
+  def recvPacket(self, headerSize: int) -> bytes:
+    data, _ = self.socket.recvfrom(self.blockSize + headerSize)
+    return data
+
+  def createRRQ(self, nombrefichero: str, modo: str) -> bytearray:
+    packet = b''
+    packet += b'01'
+    packet += nombrefichero.encode()
+    packet += b'\0'
+    packet += modo.encode()
+    packet += b'\0'
+    return packet
+
+  def createWRQ(self, nombrefichero: str, modo: str) -> bytearray:
+    packet = b''
+    packet += b'02'
+    packet += nombrefichero.encode()
+    packet += b'\0'
+    packet += modo.encode()
+    packet += b'\0'
+    return packet
+
+  def createDATA(self, numbloque: int, data: bytearray) -> bytearray:
+    packet = b''
+    packet += b'03'
+    packet += numbloque.to_bytes(2, 'big')
+    packet += data
+    return packet
+
+  def createACK(self, numbloque: int) -> bytearray:
+    packet = b''
+    packet += b'04'
+    packet += numbloque.to_bytes(2, 'big')
+    return packet
+
+  def createERROR(self, codigoerror: str, mensajeerror: str) -> bytearray:
+    packet = b''
+    packet += b'05'
+    packet += codigoerror.encode()
+    packet += mensajeerror.encode()
+    packet += b'\0'
+    return packet
+
+
+def main():
+  client = Client('localhost', 12000, 512, 1000)
+  # client.GET('a.txt', 'netascii')
+  client.PUT('a.txt', 'netascii')
 
 if __name__ == "__main__":
   main()
--- UDPServer_manresa-alfredo_bermejo-xavier_entrega_3.py
+++ UDPServer_manresa-alfredo_bermejo-xavier_entrega_3.py
@@ -1,121 +1,134 @@
 from socket import *
 import os
 
+class Server:
+
+  def __init__(self, serverPort, blockSize, timeout):
+    self.socket = socket(AF_INET, SOCK_DGRAM)
+    self.serverPort = serverPort
+    self.blockSize = blockSize
+    self.timeout = timeout
+    self.clientAddr = None
+    self.socket.bind(('', self.serverPort))
+
+  def setupHandler(self):
+    data = self.recvPacket(4)
+    packetType = data[:2]
+    filename = data.decode('utf-8')[2:2+data[2:].decode().index('\0')]
+
+    if packetType == b'01':
+      print('GET')
+      self.handleGET(filename)
+      print('Ended sending file:', filename, 'to client:', self.clientAddr)
+    elif packetType == b'02':
+      print('PUT')
+      self.handlePUT(filename)
+      print('Ended reciving file:', filename, 'from client:', self.clientAddr)
+
+
+  def handleGET(self, filename):
+
+    contador = 1
+
+    file = open(filename, "rb")
+
+    data = file.read(self.blockSize)
+
+    while data != b'':
+      packet = self.createDATA(contador, data)
+      self.sendPacket(packet)
+
+      clientACK = self.recvPacket(4)
+
+      print('Num seq:', contador)
+      contador = (contador % pow(2, 16)) + 1
+
+      data = file.read(self.blockSize)
+
+      if data == b'' and self.extraEmpty(filename):
+        packet = self.createDATA(contador, b'')
+        self.sendPacket(packet)
+
+  def handlePUT(self, filename):
+    packet = self.createACK(0)
+    self.sendPacket(packet)
+
+    
+
+    file = open(filename + ".2", 'wb')
+
+    while True:
+
+      data = self.recvPacket(4)
+
+      packetNum = int.from_bytes(data[2:4], 'big')
+      packetData = data[4:]
+
+      file.write(packetData)
+
+      packet = self.createACK(packetNum + 1)
+      self.sendPacket(packet)
+
+
+      if len(data) < (self.blockSize + 4):
+        file.close()
+        break
+
+
+  def extraEmpty(self, filename: str) -> bool:
+    return (os.path.getsize(filename) % self.blockSize) == 0
+
+  def sendPacket(self, packet: bytearray) -> None:
+    self.socket.sendto(packet, self.clientAddr)
+
+  def recvPacket(self, headerSize: int) -> bytes:
+    data, clientAddr = self.socket.recvfrom(self.blockSize + headerSize)
+    self.clientAddr = clientAddr
+    return data
+
+  def createRRQ(self, nombrefichero: str, modo: str) -> bytearray:
+    packet = b''
+    packet += b'01'
+    packet += nombrefichero.encode()
+    packet += b'0'
+    packet += modo.encode()
+    packet += b'0'
+    return packet
+
+  def createWRQ(self, nombrefichero: str, modo: str) -> bytearray:
+    packet = b''
+    packet += b'02'
+    packet += nombrefichero.encode()
+    packet += b'0'
+    packet += modo.encode()
+    packet += b'0'
+    return packet
+
+  def createDATA(self, numbloque: int, data: bytearray) -> bytearray:
+    packet = b''
+    packet += b'03'
+    packet += numbloque.to_bytes(2, 'big')
+    packet += data
+    return packet
+
+  def createACK(self, numbloque: int) -> bytearray:
+    packet = b''
+    packet += b'04'
+    packet += numbloque.to_bytes(2, 'big')
+    return packet
+
+  def createERROR(self, codigoerror: str, mensajeerror: str) -> bytearray:
+    packet = b''
+    packet += b'05'
+    packet += codigoerror.encode()
+    packet += mensajeerror.encode()
+    packet += b'0'
+    return packet
+
 
 def main():
+  server = Server(12000, 512, 1000)
+  server.setupHandler()
 
-  serverPort = 12000#input('puerto servidor: ')
-
-  CHUNK_SIZE = 512  # default value to get def size on first packet
-
-  serverSocket = socket(AF_INET, SOCK_DGRAM)
-  serverSocket.bind(('', serverPort))
-
-  print ('El Servidor esta listo para recibir')
-  while True:
-
-    clientMSG, clientAddr = serverSocket.recvfrom(CHUNK_SIZE)
-    CHUNK_SIZE = int(clientMSG.decode())
-
-    clientMSG, clientAddr = serverSocket.recvfrom(CHUNK_SIZE)
-   
-    # Enviar info al cliente
-    if clientMSG[0:2].decode() == '01':
-      name = clientMSG.decode()[2:2+clientMSG[2:].decode().index('0')]
-      contador = 1
-      file = open(name, "rb")
-      data = file.read(CHUNK_SIZE - 4)
-      while data:
-        serverSocket.sendto(createDATA(contador, data), clientAddr)
-        contador = (contador % 65535) + 1
-        clientACK, _ = serverSocket.recvfrom(CHUNK_SIZE)
-        print(clientACK)
-        data = file.read(CHUNK_SIZE - 4)
-        
-
-      if (os.path.getsize(name) % (CHUNK_SIZE - 4)) == 0:
-        serverSocket.sendto(createDATA(contador, ''), clientAddr)
-
-      file.close()
-
-    # recibir info del cliente
-    elif clientMSG[0:2].decode() == '02':
-      name = clientMSG.decode()[2:2+clientMSG[2:].decode().index('0')]
-
-      file = open(name + ".2", "wb")
-
-      serverSocket.sendto(createACK('0'), clientAddr)
-
-      
-      while True:
-
-        data, _ = serverSocket.recvfrom(CHUNK_SIZE)
-
-        packetType = data[:2]
-
-        if packetType == b'03':  # DATA
-          # guardamos datos
-          file.write(data[4:])
-          serverSocket.sendto(createACK(data[2:4]), clientAddr)
-
-        elif packetType == b'05':  # ERR
-          # error
-          file.close()
-          print('Code: Message:',  data[2:4].decode(),  data[4:].decode())
-
-        print("packet size recv: ", len(data))
-
-        print(len(data))
-        if len(data) < CHUNK_SIZE:
-          # sabemos que es el ultimo paquete enviado para que se para de guardar en el archivo
-          file.close()
-          break
-
-    # no es un put o un get en el momento adecuado
-    else:
-        pass
-
-
-
-
-def createRRQ(nombrefichero, modo):
-  packet = b''
-  packet += b'01'
-  packet += nombrefichero.encode()
-  packet += b'0'
-  packet += modo.encode()
-  packet += b'0'
-  return packet
-
-def createWRQ(nombrefichero, modo):
-  packet = b''
-  packet += b'02'
-  packet += nombrefichero.encode()
-  packet += b'0'
-  packet += modo.encode()
-  packet += b'0'
-  return packet
-
-def createDATA(numbloque, data):
-  packet = b''
-  packet += b'03'
-  packet += "{:02d}".format(numbloque).encode()
-  packet += data  # is already encoded
-  return packet
-    
-def createACK(numbloque):
-  packet = b''
-  packet += b'04'
-  packet += "{:02d}".format(int(numbloque)).encode()
-  return packet
-
-def createERROR(codigoerror, mensajeerror):
-  packet = b''
-  packet += b'05'
-  packet += codigoerror.encode()
-  packet += mensajeerror.encode()
-  packet += b'0'
-  return packet
-  
 if __name__ == "__main__":
   main()
